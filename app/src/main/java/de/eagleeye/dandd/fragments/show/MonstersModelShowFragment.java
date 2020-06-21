459236949c69ed6f03ba5b1f7e1098a7bde2e8f5
christoph <chrisschlueter@t-online.de>
2020-06-21 20:29:36 +0200

    Load Model File from Database (Permission Denied)

--- app/src/main/java/de/eagleeye/dandd/fragments/show/MonstersModelShowFragment.java
+++ app/src/main/java/de/eagleeye/dandd/fragments/show/MonstersModelShowFragment.java
@@ -1,5 +1,6 @@
 package de.eagleeye.dandd.fragments.show;
 
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,14 +26,35 @@ import java.io.File;
 
 import de.eagleeye.dandd.R;
 import de.eagleeye.dandd.fragments.base.BaseDataShowFragment;
+import de.eagleeye.dandd.sql.BasicSQLiteHelper;
+import de.eagleeye.dandd.sql.SQLRequest;
 
 public class MonstersModelShowFragment extends BaseDataShowFragment {
     private ArFragment arFragment;
     private ModelRenderable modelRenderable;
 
+    private File file;
+    private float scale;
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        file = new File("");
+        scale = 1.0f;
+
+        Bundle args = getArguments();
+        if(args != null) {
+            BasicSQLiteHelper db = new BasicSQLiteHelper(getActivity(), "data.db");
+            db.query(new SQLRequest("SELECT files.path, monsters.modelScale FROM monsters LEFT JOIN files ON monsters.modelId=files.id AND monsters.modelSourceId=files.sourceId WHERE monsters.id=" + args.get("id") + " AND monsters.sourceId=" + args.get("sourceId") + ";",
+                    cursor -> {
+                        if (cursor != null && getActivity() != null && cursor.moveToFirst()){
+                            file = new File(getActivity().getFileStreamPath(""), cursor.getString(0));
+                            scale = cursor.getFloat(1);
+                        }
+                    }));
+        }
+
         if(getActivity() != null) {
             arFragment = (ArFragment) getChildFragmentManager().findFragmentById(R.id.fragment_show_monster_model_fragment);
             setupModel();
@@ -51,26 +73,27 @@ public class MonstersModelShowFragment extends BaseDataShowFragment {
     }
 
     private void setupModel(){
-        ModelRenderable.builder().setSource(getActivity(), RenderableSource.builder().setSource(
-                getActivity(),
-                Uri.fromFile(new File(getActivity().getFileStreamPath("models"), "Dragon.glb")),
-                RenderableSource.SourceType.GLB)
-                .setScale(0.01f)  // Scale the original model to 50%.
-                .setRecenterMode(RenderableSource.RecenterMode.ROOT)
-                .build())
-                .build()
-                .thenAccept(renderable -> {
-                    modelRenderable = renderable;
-                })
-                .exceptionally(
-                        throwable -> {
-                            Toast toast = Toast.makeText(getActivity(), "Unable to load renderable ", Toast.LENGTH_LONG);
-                            toast.setGravity(Gravity.CENTER, 0, 0);
-                            toast.show();
-                            Log.e(MonstersModelShowFragment.this.getClass().getSimpleName(), "Unable to render Model", throwable);
-                            return null;
-                        });
-
+        if(getActivity() != null) {
+            ModelRenderable.builder().setSource(getActivity(), RenderableSource.builder().setSource(
+                    getActivity(),
+                    Uri.fromFile(file),
+                    RenderableSource.SourceType.GLB)
+                    .setScale(scale)
+                    .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+                    .build())
+                    .build()
+                    .thenAccept(renderable -> {
+                        modelRenderable = renderable;
+                    })
+                    .exceptionally(
+                            throwable -> {
+                                Toast toast = Toast.makeText(getActivity(), "Unable to load renderable ", Toast.LENGTH_LONG);
+                                toast.setGravity(Gravity.CENTER, 0, 0);
+                                toast.show();
+                                Log.e(MonstersModelShowFragment.this.getClass().getSimpleName(), "Unable to render Model", throwable);
+                                return null;
+                            });
+        }
     }
 
     private void setupPlane(){
